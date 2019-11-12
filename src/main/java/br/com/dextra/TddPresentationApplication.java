e0e5f585b454c3f255e9cd95275db2b474f2dd5c
José Bonifácio <jose.bonifacio@dextra-sw.com>
2019-11-12 19:14:41 -0300

    First tests

---
+++ src/main/java/br/com/dextra/TddPresentationApplication.java
@@ -0,0 +1,13 @@
+package br.com.dextra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddPresentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dextra/TddPresentationApplicationTests.java
@@ -0,0 +1,24 @@
+package br.com.dextra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPresentationApplicationTests {
+
+	@Test
+	public void testFileLoad() {
+		
+	}
+
+	@Test
+	public void testFileWrite() {
+
+	}
+
+	@Test
+	public void testFileContentChange() {
+
+	}
+
+}
