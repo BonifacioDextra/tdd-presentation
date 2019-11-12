693d16462784175adbb71fe867e44be3bf9bdd95
José Bonifácio <jose.bonifacio@dextra-sw.com>
2019-11-12 20:20:05 -0300

    Implementação 1

--- src/test/java/br/com/dextra/TddPresentationApplicationTests.java
+++ src/test/java/br/com/dextra/TddPresentationApplicationTests.java
@@ -1,24 +1,39 @@
 package br.com.dextra;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class TddPresentationApplicationTests {
 
+	private Parser parser;
+
+	@BeforeEach
+	public void init() {
+		parser = new Parser();
+		parser.loadFile("src/test/resources/static/test.txt");
+	}
+
 	@Test
 	public void testFileLoad() {
-		
+		assertEquals(parser.getContent(), "12345");
 	}
 
 	@Test
 	public void testFileWrite() {
-
+		String filePath = "src/test/resources/static/test2.txt";
+		parser.writeFile(filePath);
+		parser.loadFile(filePath);
+		assertEquals(parser.getContent(), "12345");
 	}
 
 	@Test
 	public void testFileContentChange() {
-
+		parser.parse();
+		assertEquals(parser.getContent(), "54321");
 	}
 
 }
